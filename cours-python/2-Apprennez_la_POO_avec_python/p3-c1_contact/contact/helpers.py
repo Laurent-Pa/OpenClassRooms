24a5df5feb84228da5e12a87d41feee13dd18246
laurent papinaud <laurent.papinaud@gmail.com>
2025-06-24 10:53:29 +0200

    refactor contact system by text or owl

---
+++ cours-python/2-Apprennez_la_POO_avec_python/p3-c1_contact/contact/helpers.py
@@ -0,0 +1,10 @@
+"""Donne les methode d'aides pour l'utilisation du system de contact."""
+
+def verify_address(address):
+    """Fausse fonction qui retourne True."""
+    return True
+
+
+def validate_phone(phone_number):
+    """Retourne True par défaut."""
+    return True
--- cours-python/2-Apprennez_la_POO_avec_python/p3-c1_contact/main.py
+++ cours-python/2-Apprennez_la_POO_avec_python/p3-c1_contact/main.py
@@ -1,15 +1,16 @@
 """Point d'entrée"""
 
 from contact.user import User
+from contact.textcontact import TextContactSystem
+from contact.owlcontact import OwlContactSystem
 
 def main():
     """Main function."""
-    alice = User("Alice")
-    bob = User("Bob")
-    # alice = User("Alice",TextContactSystem("0102030405"))
-    # bob = User("Bob", OwlContactSystem("4 Privet Drive"))
+    alice = User("Alice", TextContactSystem("0102030405"))
+    bob = User("Bob", OwlContactSystem("4 Privet Drive"))
 
     print(f"Utilisateur 1: {alice.get_name()}")
+    print(f"Utilisateur 2: {bob.get_name()}")
 
 
 # Exécuter le programme
