654dd044ff9bd1289b716001a6e0a537052d4795
laurent papinaud <laurent.papinaud@gmail.com>
2025-06-26 10:32:57 +0200

    feat(main) display list of films, friends and random lent film

--- cours-python/2-Apprennez_la_POO_avec_python/p3-c2_films/main.py
+++ cours-python/2-Apprennez_la_POO_avec_python/p3-c2_films/main.py
@@ -3,6 +3,7 @@
 # l'utilisation de pprint permet de mieux afficher les conteneurs.
 from pprint import pprint
 
+from films.friend import FriendCleaner
 from films.library import Library
 
 def main():
@@ -10,9 +11,33 @@ def main():
     library = Library()
 
     films = library.films
+    friends = FriendCleaner().list_from_data(library)
 
     print("Tous mes films:")
     pprint(films)
+
+    print()
+    print("Tous mes amis:")
+    pprint(friends)
+    print()
+
+    library.sort_by_type()
+    print("Mes films triés par type:")
+    pprint(films)
+    print()
+
+    film = library.get_random_choice()
+    print(f"Film récupéré au hasard: {film}")
+    print()
+
+    films_lent = library.get_films_lent()
+    print("J'ai prêté ces films:")
+    pprint(films_lent)
+    print()
+
+    for film in films_lent:
+        print(f"Le film '{film.name}' est chez", film.where)
+
 
 if __name__ == "__main__":
     main()
