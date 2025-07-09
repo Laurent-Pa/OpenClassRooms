8470aa20d57c37ca6a7f6a3fe75474418163a94a
laurent papinaud <laurent.papinaud@gmail.com>
2025-07-09 15:32:10 +0200

    concatenation of string

--- cours-Csharp/P1/P1C3.1/program.cs
+++ cours-Csharp/P1/P1C3.1/program.cs
@@ -6,3 +6,15 @@ double reponse = numerateur / (double) denominateur;
 
 // Afficher le résultat
 Console.WriteLine(numerateur + " / " + denominateur + " = " + reponse);
+
+
+// concatenation de chaînes de caractère
+string premiereVillePreferee = "New York";
+string secondeVillePreferee = "Buenos Aires";
+
+string favoris = premiereVillePreferee + " " + secondeVillePreferee;
+// -> «·New YorkBuenos Aires·»
+Console.WriteLine(favoris);
+
+// On peut déclarer une variable sans lui donner de type --> var = "" ou 3
+// le compilateur va déduire s'il s'agit d'une string ou int
