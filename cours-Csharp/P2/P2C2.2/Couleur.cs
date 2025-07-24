b53b96747298bfb53b6ee83dd7acde08783c8de4
laurent papinaud <laurent.papinaud@gmail.com>
2025-07-24 19:00:34 +0200

    exercice instructions added

---
+++ cours-Csharp/P2/P2C2.2/Couleur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2C2._2
+{
+    public enum Couleur
+    {
+        Bleu,
+        Vert,
+        Rouge,
+        Jaune
+    }
+}
