e1804092fc9e27f1bd3f8c44b9711817bf6ded84
laurent papinaud <laurent.papinaud@gmail.com>
2025-07-25 11:49:54 +0200

    try catch finally added to handle exceptions in case of wrong input in terminal by user

--- cours-Csharp/P2/P2C4.1/Program.cs
+++ cours-Csharp/P2/P2C4.1/Program.cs
@@ -5,24 +5,39 @@ using P2C4._1;
 
 // Le programme affiche la température moyenne à partir des valeurs fournies comme arguments en ligne de commande
 
-List<int> temperaturesEnregistreDegresCelcius = new List<int>();
+try
+{
+    List<int> temperaturesEnregistreDegresCelcius = new List<int>();
 
-// Remplir la liste à partir des valeurs fournies comme arguments en ligne de commande
-foreach (string stringRepresentationTemperature in args)
-{
-    int temperature = int.Parse(stringRepresentationTemperature);
-    temperaturesEnregistreDegresCelcius.Add(temperature);
-}
+    // Remplir la liste à partir des valeurs fournies comme arguments en ligne de commande
+    foreach (string stringRepresentationTemperature in args)
+    {
+        int temperature = int.Parse(stringRepresentationTemperature);
+        temperaturesEnregistreDegresCelcius.Add(temperature);
+    }
 
-// Gérer une liste vide
-if (temperaturesEnregistreDegresCelcius.Count == 0)
-{
-    Console.WriteLine("Impossible de calculer la moyenne avec une liste vide !");
-}
-else
-{
     // Calculer et afficher la température moyenne
     int temperatureMoyenne =
         MathSimple.CalculMoyenne(temperaturesEnregistreDegresCelcius);
     Console.WriteLine("La température moyenne est " + temperatureMoyenne);
 }
+
+catch (FormatException e)
+{
+    Console.WriteLine("Les températures fournies doivent être des nombres");
+    // l'exercice propose Environment.Exit(-1) mais cela ferme directement le programme
+    throw; // permet de remonter l'exception d'un niveau supérieur, finally pourra être executé
+}
+
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("Vous devez fournir au moins une température");
+    throw;
+}
+
+finally
+{
+    // Ce bloc s'exécute avec "throw" mais PAS avec "Environment.Exit(-1)"
+    Console.WriteLine("Fin du programme");
+}
+
