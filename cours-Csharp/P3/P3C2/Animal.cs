cc1e8c5abb1bd362810c4a323e35e1a958ecf7f0
laurent papinaud <laurent.papinaud@gmail.com>
2025-07-26 16:54:29 +0200

    first static method called from Main()

---
+++ cours-Csharp/P3/P3C2/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3C2
+{
+    public class Animal
+    {
+        //TODO. Il manque un mot-clé ici (static) pour pouvoir appeler la methode sur la classe Animal
+        private static int total = 0;
+
+        public void Incrementer()
+        {
+            Animal.total += 1;
+        }
+
+        public static void AfficherNom()
+        {
+            string nom = "Rocky";
+            Console.WriteLine(nom);
+        }
+    }
+}
---
+++ cours-Csharp/P3/P3C2/Program.cs
@@ -0,0 +1,15 @@
+﻿// TODO : exécuter la fonction AfficherNom
+
+using System;
+
+namespace P3C2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Animal poisson_rouge = new Animal();
+            Animal.AfficherNom();
+        }
+    }
+}
